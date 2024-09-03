3157da1b9dd42fbdfa70f14f81e8e83a30c1d6cc
Mathews Baby <mathewskb@gmail.com>
2024-09-03 08:42:53 +0530

    End of Section 3

---
+++ client/src/app/app.component.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from '@angular/common/http';
+import { Component, inject, OnInit } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+
+@Component({
+  selector: 'app-root',
+  standalone: true,
+  imports: [RouterOutlet],
+  templateUrl: './app.component.html',
+  styleUrl: './app.component.css'
+})
+export class AppComponent implements OnInit {
+  
+  title = 'client';
+  http = inject(HttpClient);
+  users : any;
+
+  ngOnInit(): void {
+
+    this.http.get('https://localhost:5001/api/users').subscribe({
+      next : (resp) => { this.users = resp ;},
+      error : (err) => {console.log(err)},
+      complete : () => {console.log('completed')}
+    });
+    
+  }
+}
