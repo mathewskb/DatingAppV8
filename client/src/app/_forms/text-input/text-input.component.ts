91aa37b2423d9ebe88ed7a521af8ea4261cbdf78
Mathews Baby <mathewskb@gmail.com>
2025-03-27 16:51:57 +0530

    Section 12 - 140(Reusable Component) Completed

---
+++ client/src/app/_forms/text-input/text-input.component.ts
@@ -0,0 +1,39 @@
+import { getSupportedInputTypes } from '@angular/cdk/platform';
+import { NgIf } from '@angular/common';
+import { publishFacade } from '@angular/compiler';
+import { Component, input, Self } from '@angular/core';
+import { ControlValueAccessor, FormControl, NgControl, ReactiveFormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-text-input',
+  standalone: true,
+  imports: [ReactiveFormsModule, NgIf],
+  templateUrl: './text-input.component.html',
+  styleUrl: './text-input.component.css'
+})
+export class TextInputComponent implements ControlValueAccessor {
+
+  label = input<string>('');
+  type = input<string>('text');
+
+  constructor(@Self() public ngControl: NgControl){
+    this.ngControl.valueAccessor = this;
+  }
+
+get control() : FormControl{
+  return this.ngControl.control as FormControl;
+}
+
+  writeValue(obj: any): void {
+
+  }
+
+  registerOnChange(fn: any): void {
+
+  }
+
+  registerOnTouched(fn: any): void {
+
+  }
+
+}
