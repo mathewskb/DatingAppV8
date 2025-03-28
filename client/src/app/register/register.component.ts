6683ec5aa9c108ace4aacda4f658e015afe2a2eb
Mathews Baby <mathewskb@gmail.com>
2025-03-28 14:56:06 +0530

    Section 12 - 143 ( Adding reusable date component) completed

--- client/src/app/register/register.component.ts
+++ client/src/app/register/register.component.ts
@@ -1,14 +1,15 @@
 import { Component, inject, input, OnInit, output } from '@angular/core';
-import { AbstractControl, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormControl, FormGroup, FormsModule, ReactiveFormsModule, ValidatorFn, Validators } from '@angular/forms';
 import { AccountService } from '../_services/account.service';
 import { ToastrService } from 'ngx-toastr';
 import { JsonPipe, NgIf } from '@angular/common';
 import { TextInputComponent } from "../_forms/text-input/text-input.component";
+import { DatePickerComponent } from "../_forms/date-picker/date-picker.component";
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [ReactiveFormsModule, JsonPipe, NgIf, TextInputComponent],
+  imports: [ReactiveFormsModule, JsonPipe, TextInputComponent, DatePickerComponent],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
@@ -20,16 +21,25 @@ export class RegisterComponent implements OnInit {
   accountService = inject(AccountService);
   cancelRegister = output<boolean>();
   private toastr = inject(ToastrService);
+  private fb = inject(FormBuilder)
+
+  maxDate = new Date();
 
   ngOnInit(): void {
     this.initializeForm();
+    this.maxDate.setFullYear(this.maxDate.getFullYear() - 18);
   }
 
   initializeForm() {
-    this.registerForm = new FormGroup({
-      username: new FormControl('', [Validators.required]),
-      password: new FormControl('', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]),
-      confirmPassword: new FormControl('', [Validators.required, this.matchValues('password')])
+    this.registerForm = this.fb.group({
+      gender : ['male'],
+      username: ['', [Validators.required]],
+      knownAs : ['', Validators.required],
+      dateOfBirth : ['', Validators.required],
+      city : ['', Validators.required],
+      country : ['', Validators.required],
+      password: ['', [Validators.required, Validators.minLength(4), Validators.maxLength(8)]],
+      confirmPassword: ['', [Validators.required, this.matchValues('password')]]
     })
 
     this.registerForm.controls['password'].valueChanges.subscribe({
